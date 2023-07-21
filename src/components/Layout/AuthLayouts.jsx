5c68e139229379f3511850da88fc393f23685b34
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-07-21 11:26:18 +0700

    mengganti kondisi sebelumnya dengan if else

--- src/components/Layout/AuthLayouts.jsx
+++ src/components/Layout/AuthLayouts.jsx
@@ -12,21 +12,7 @@ const AuthLayouts = (props) => {
                 <h1 className="text-red-600 text-3xl font-bold mb-2">{title}</h1>
                 <p className="font-medium text-slate-500 mb-8">Welcome, Please enter your details!</p>
                 {children}
-                <p className="text-sm mt-5 text-center">
-                    {type === "login" ? "Don't have an account? " : "Already have an account? "} 
-                    
-                    {type == "login" && (
-                        <Link className="font-bold  text-red-600" to="/register">
-                            Register
-                        </Link>
-                    )}
-
-                    {type == "register" && (
-                        <Link className="font-bold  text-red-600" to="/login">
-                            Login
-                        </Link>
-                    )}
-                </p>
+                <navigation />
             </div>
         </div>
     )
